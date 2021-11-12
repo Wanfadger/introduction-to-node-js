92b64ebf25391dbd8e01a9a167e2b9e76f1a435e
wanfadger <galiwango.fahad@gmail.com>
2021-11-12 20:46:06 +0300

    created and started a simple web server

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
 const fs = require("fs");
+const http = require("http")
 
 //blocking sysnchronous way
 
-const textIn = fs.readFileSync("txt/input.txt", "utf-8");
+// const textIn = fs.readFileSync("txt/input.txt", "utf-8");
 
-console.log(textIn);
+// console.log(textIn);
 
-const textOut = `This is what we know about Avacado: ${textIn}`;
-console.log(textOut);
+// const textOut = `This is what we know about Avacado: ${textIn}`;
+// console.log(textOut);
 
-fs.writeFileSync("txt/output.txt", textOut);
-console.log("Written Successfully");
+// fs.writeFileSync("txt/output.txt", textOut);
+// console.log("Written Successfully");
 
 ///none blocking asycn operation
-fs.readFile("txt/input.txt", "utf-8", (error, data) => {
-  console.log(data);
+// fs.readFile("txt/input.txt", "utf-8", (error, data) => {
+//   console.log(data);
+// });
+
+// console.log("after");
+
+// fs.readFile("txt/start.txt", "utf-8", (error, data1) => {
+//   fs.readFile(`txt/${data1}.txt`, "utf-8", (error, dat2) => {
+//     fs.readFile(`txt/append.txt`, "utf-8", (error, data3) => {
+//       fs.writeFile(`txt/final.txt`, `${dat2} . \n ${data3}`, (error, data) => {
+//         if (error) console.log("something wrong happened");
+//         console.log("success");
+//       });
+//     });
+//   });
+// });
+
+
+/////////////////////
+ //CREATING SERVER
+
+const server = http.createServer((req, res) => {
+  res.end("Hello World");
 });
 
-console.log("after");
+server.listen('8000', '127.0.0.1', () => {
+    console.log('server started on port 8000')
+})
+ 
 
-fs.readFile("txt/start.txt", "utf-8", (error, data1) => {
-  fs.readFile(`txt/${data1}.txt`, "utf-8", (error, dat2) => {
-    fs.readFile(`txt/append.txt`, "utf-8", (error, data3) => {
-      fs.writeFile(`txt/final.txt`, `${dat2} . \n ${data3}`, (error, data) => {
-        if (error) console.log("something wrong happened");
-        console.log("success");
-      });
-    });
-  });
-});
