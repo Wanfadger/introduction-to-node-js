65c3fb90609e15f63cef9b11d315eee57402afb0
wanfadger <galiwango.fahad@gmail.com>
2021-11-14 08:32:56 +0300

    passed data to listeners

--- 2-how-node-works/starter/events.js
+++ 2-how-node-works/starter/events.js
@@ -1,8 +1,11 @@
-const EventEmitter = require("events")
+const EventEmitter = require("events");
 
-const myEvent = new EventEmitter()
+const myEvent = new EventEmitter();
 
-myEvent.on("onSale" , () => console.log("There was a new sale"))
-myEvent.on("onSale", () => console.log("Customer name Wanfadger"))
+myEvent.on("onSale", () => console.log("There was a new sale"));
+myEvent.on("onSale", () => console.log("Customer name Wanfadger"));
 
-myEvent.emit("onSale")
+myEvent.on("onSale", (stock) => console.log(`There are ${stock} items left in the stock`));
+
+myEvent.emit("onSale" , 10);
+//passing data on  event emit
