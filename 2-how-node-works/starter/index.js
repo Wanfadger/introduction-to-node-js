079985fab0f7be9acf793d686c711a50ce5ab069
wanfadger <galiwango.fahad@gmail.com>
2021-11-14 08:16:53 +0300

    worked with default  threads
    increased and decreased thread pool

--- 2-how-node-works/starter/index.js
+++ 2-how-node-works/starter/index.js
@@ -1,7 +1,13 @@
 const fs = require("fs")
+const crypto = require("crypto")
+
+//changing the thread pool
+process.env.UV_THREADPOOL_SIZE = 10
 
 setTimeout(() => console.log("Timer 1 finished "), 0)
 setImmediate(() => console.log("Intermediate 1 finished"))
+
+let start = Date.now()
 
 fs.readFile("test-file.txt", "utf-8", (error, data) => {
     console.log("I/O finished")
@@ -11,6 +17,29 @@ fs.readFile("test-file.txt", "utf-8", (error, data) => {
     setImmediate(() => console.log("Intermediate 1finished"));
 
     process.nextTick(() => console.log("next Tick"))
+
+
+    crypto.pbkdf2("password", "salt", 10000, 1000, 'sha512', () => {
+        console.log(`${Date.now() - start}  password encrypted`)
+    });
+
+        crypto.pbkdf2("password", "salt", 10000, 1000, "sha512", () => {
+          console.log(`${Date.now() - start}  password encrypted`);
+        });
+        crypto.pbkdf2("password", "salt", 10000, 1000, "sha512", () => {
+          console.log(`${Date.now() - start}  password encrypted`);
+        });
+        crypto.pbkdf2("password", "salt", 10000, 1000, "sha512", () => {
+          console.log(`${Date.now() - start}  password encrypted`);
+        });
+        crypto.pbkdf2("password", "salt", 10000, 1000, "sha512", () => {
+          console.log(`${Date.now() - start}  password encrypted`);
+        });
+        crypto.pbkdf2("password", "salt", 10000, 1000, "sha512", () => {
+          console.log(`${Date.now() - start}  password encrypted`);
+        });
+
+
 })
 
 console.log("Hello from the top level code")
