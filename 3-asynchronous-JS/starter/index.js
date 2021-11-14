f8c3759e2c6490d5ac70174716baefa9dec4c5d2
wanfadger <galiwango.fahad@gmail.com>
2021-11-14 14:54:35 +0300

    added a promise

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,20 +1,18 @@
-const fs = require("fs")
-const superagent = require("superagent")
+const fs = require("fs");
+const superagent = require("superagent");
 
 ///read dog.txt file
 
 fs.readFile(`${__dirname}/dog.txt`, "utf-8", (e, data) => {
-    console.log(data)
+  console.log(data);
 
-    //dog.ceo/api/breed/
-    superagent.get(
-      `https://dog.ceo/api/breed/${data}/images/random`,
-      (error, response) => {
-          if (error) return console.log(error.message);
-          
-          fs.writeFile("dogImage.txt", response.body.message, (err) => {
-              if (err) return console.log(err.message);
-          })
-      }
-    );
-})
+  //dog.ceo/api/breed/
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then((res) => {
+      fs.writeFile("dogImage.txt", response.body.message, (err) => {
+        if (err) return console.log(err.message);
+      });
+    })
+    .catch((error) => console.log(error.message));
+});
