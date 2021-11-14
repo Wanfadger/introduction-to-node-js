e4dff03783ade6ffe7ec73553ba959a55396e8c9
wanfadger <galiwango.fahad@gmail.com>
2021-11-14 15:14:18 +0300

    added multiple chains on promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -3,16 +3,42 @@ const superagent = require("superagent");
 
 ///read dog.txt file
 
-fs.readFile(`${__dirname}/dog.txt`, "utf-8", (e, data) => {
-  console.log(data);
+// fs.readFile(`${__dirname}/dog.txt`, "utf-8", (e, data) => {
+//   console.log(data);
 
-  //dog.ceo/api/breed/
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      fs.writeFile("dogImage.txt", res.body.message, (err) => {
-        if (err) return console.log(err.message);
-      });
-    })
-    .catch((error) => console.log(error.message));
-});
+//   //dog.ceo/api/breed/
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .then((res) => {
+//       fs.writeFile("dogImage.txt", res.body.message, (err) => {
+//         if (err) return console.log(err.message);
+//       });
+//     })
+//     .catch((error) => console.log(error.message));
+// });
+
+const fileReaderPromise = () => {
+  return new Promise((ressolve, reject) => {
+    fs.readFile(`${__dirname}/dog.txt`, "utf-8", (error, data) => {
+      if (error) reject(error);
+      ressolve(data);
+    });
+  });
+};
+
+const fileWriterPromise = (image) => {
+  return new Promise((ressolve, reject) => {
+    fs.writeFile("dogImage.txt", image, (err) => {
+      if (err) reject(err);
+      ressolve("Sucessfully written");
+    });
+  });
+};
+
+fileReaderPromise()
+  .then((data) =>
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+  )
+  .then((response) => fileWriterPromise(response.body.message))
+  .then((res) => console.log(res))
+  .catch((error) => console.error(error.message));
